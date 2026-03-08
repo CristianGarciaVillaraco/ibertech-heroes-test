b0d47c6405ad656955fd10b500a17b23bb925b8f
Cristian <cristiangarciavillaraco@gmail.com>
2026-03-08 23:24:37 +0100

    fix(ci): complete karma.conf.js with jasmine framework and ChromeHeadlessCI launcher

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,23 @@
 module.exports = function (config) {
   config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+    ],
+    client: { jasmine: {} },
+    jasmineHtmlReporter: { suppressAll: true },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/heroes'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['ChromeHeadless'],
+    restartOnFileChange: true,
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
